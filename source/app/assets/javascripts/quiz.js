300ea74e209c00ff555bb6d54236d4e9794ddf98
Apprentice <apprentice@dbc23.local>
2014-02-26 11:51:56 -0800

    Refactors Quiz related methods
    Refactors by moving all quiz related methods into a module; hides some 'private' methods using the module design pattern.

--- source/app/assets/javascripts/quiz.js
+++ source/app/assets/javascripts/quiz.js
@@ -1,28 +1,36 @@
-var getQuizzes = function() {
-  $.ajax({
-    type: 'get',
-    url: '/quizzes.json'
-  })
-  .done(function(json) {
+//-----QUIZ MODULE-----
+var Quiz = (function() {
+  var _render = function(quiz) {
+      return "<div class='quiz'><h2>" + quiz.name + "</h2><div class='question'></div></div>";
+    }
+
+  var _appendQuizzes = function(json) {
     quizzes = json.quizzes
     for (var i=0; i < quizzes.length; i++) {
       quiz = quizzes[i];
-      $('.container').append(Quiz.render(quiz));
+      $('.container').append(_render(quiz));
     }
-  })
-  .fail(function() {
-    $('.container').append('Quizzes not found!');
-  })
-}
-
-var Quiz = {
-  render: function(quiz) {
-    return "<div><h2>" + quiz.name + "</h2></div>";
   }
-}
+
+  var _appendError = function() {
+    $('.container').append('Quizzes not found!');
+  }
+
+  return {
+    getQuizzes: function() {
+      $.ajax({
+        type: 'get',
+        url: '/quizzes.json'
+      })
+      .done(_appendQuizzes)
+      .fail(_appendError)
+    }
+  }
+})()
 
 
-// On document ready:
+//-----ON DOCUMENT READY-----
 $( document ).ready(function() {
-  getQuizzes();
+  session_key =
+  Quiz.getQuizzes();
 })
