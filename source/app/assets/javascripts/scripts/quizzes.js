8599c12b9d1567176b2076215f28c45925b19bdd
& <pair+dslachtman.mh120888@devbootcamp.com>
2014-02-26 12:22:15 -0800

    Creates database.yml file See list of quizzes Adds show-quizzes div to index view.

---
+++ source/app/assets/javascripts/scripts/quizzes.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+  $('#quiz-link a').on('ajax:success', Quiz.showQuizzes);
+  $('#quiz-link a').on('ajax:error', Quiz.quizFail);
+});
+
+var Quiz = {
+  showQuizzes: function(e, data) {
+    e.preventDefault();
+    $('#quiz-link').hide();
+    for (var i = 0; i < data.quizzes.length; i++) {
+      $('#show-quizzes').append("<a href='#'>" + data.quizzes[i].name + "</a>");
+    }
+    //shitWeWant = JSON.parse(data)
+  },
+  quizFail: function(e, xhr) {
+    // do shit with xhr.responseText
+  }
+}
