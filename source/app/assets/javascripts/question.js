4d1f201951d696cdab814bd1e0edf54327d4a6e0
Apprentice <apprentice@dbc23.local>
2014-02-26 18:11:22 -0800

    Converts question template into handlebars.js template
    This conversion eliminated the need for a separate choice module, choice.js has been removed.

--- source/app/assets/javascripts/question.js
+++ source/app/assets/javascripts/question.js
@@ -1,13 +1,15 @@
 //-----QUESTION MODULE-----
 var Question = (function() {
   var _render = function(question) {
-    return question.question + "<form method='post' action='/questions/" + question.question_id + "/answers.json'>" + Choice.renderAll(question.choices) + "<input type='submit' value='Submit answer'><input type='hidden' name='session_key' value='" + sessionKey + "'></form>";
+    var handlebarTemplate = $('#question').html();
+    var template = Handlebars.compile(handlebarTemplate);
+    return template(question);
+    // return question.question + "<form method='post' action='/questions/" + question.question_id + "/answers.json'>" + Choice.renderAll(question.choices) + "<input type='submit' value='Submit answer'><input type='hidden' name='session_key' value='" + sessionKey + "'></form>";
   }
 
   var _appendQuestion = function(json) {
-    question = json.question;
-    quizId = question.choices[0].quiz_id;
-    $("#quiz" + quizId + " .question").html(_render(question));
+    quizId = json.question.choices[0].quiz_id;
+    $("#quiz" + quizId + " .question").html(_render(json));
   }
 
   var _appendError = function() {
