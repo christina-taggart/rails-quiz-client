3778d7fc0b39e849263a32798376003278e388d2
Apprentice <apprentice@dbc21.local>
2014-02-26 17:17:11 -0800

    andy and emmanuel working quiz template, no styling yet

--- source/app/assets/javascripts/quizzes.js
+++ source/app/assets/javascripts/quizzes.js
@@ -22,11 +22,11 @@ $(function() {
 
 var Router = (function() {
   function init() {
-    // bind our events here  
-    $('.quiz-list').on('click', '.quiz-link', QuestionsController.getNextQuestion)
+    // bind our events here
+    $('.quiz-list').on('click', '.quiz-link', QuestionsController.getFirstQuestion)
     $('.container').on('click', '.choice-link', QuestionsController.displayResult)
   }
-  
+
   return {
     init: init
   }
@@ -45,25 +45,25 @@ var QuizzesController = (function() {
       alert("Uh-oh!")
     })
   }
-  
+
   function render(quiz) {
     var $quizTemplate = $($('#quiz-template').html())
     var $quizAnchor = $quizTemplate.find('a')
     $quizAnchor.html(quiz.name).attr('href', '/quizzes/'+quiz.quiz_id).data('id', quiz.quiz_id)
     $('.quiz-list').append($quizTemplate)
   }
-  
+
   return {
     getQuizzes: getQuizzes
   }
 }())
 
 var QuestionsController = (function() {
-  function getNextQuestion(event) {
+  function getFirstQuestion(event) {
     event.preventDefault()
     $.ajax({
         type: 'get',
-        url: $(event.target).attr('href') + '/questions/next.json',
+        url: $(this).attr('href') + '/questions/next.json',
         data: { session_key: sessionKey }
     })
     .done(function(data){
@@ -71,31 +71,72 @@ var QuestionsController = (function() {
     })
   }
 
-  
-  
+  function getNextQuestion(event, url) {
+     event.preventDefault()
+    $.ajax({
+        type: 'get',
+        url: url,
+        data: { session_key: sessionKey }
+    })
+    .done(function(data){
+       renderQuestion(data.question)
+    })
+  }
+
+
+
+
+  function displayResult(event) {
+    event.preventDefault()
+
+    $.ajax({
+      type: 'post',
+      url: $(event.target).attr('href'),
+      data: { session_key: sessionKey, choice_id: $(this).attr('id')  }
+    })
+    .done(function(data){
+      renderResults(data)
+      var quiz_id = data.status.quiz_id
+      var url = "quizzes/" + quiz_id + "/questions/next.json"
+      if (data.status.correct == true && data.status.more_questions == true){
+        getNextQuestion(event, url)
+    } else if (data.status.correct == true && data.status.more_questions != true) {
+        $(".container").html("GOOD JERB")
+        sessionKey += "x"
+    }
+    })
+  }
+
+
+
   function renderQuestion(question) {
-    // for each choice, renderChoice()
-    // var $quizTemplate = $($('#quiz-template').html())
-    // var $quizAnchor = $quizTemplate.find('a')
-    // $quizAnchor.html(quiz.name).attr('href', '/quizzes/'+quiz.quiz_id).data('id', quiz.quiz_id)
-    // $('.quiz-list').append($quizTemplate)
     var $questionTemplate = $($('#question-template').html())
-    var $questionField = $questionTemplate.find('p')
+    var $questionField = $($questionTemplate[0])
     $questionField.html(question.question).data('id', question.question_id)
     for (var i in question.choices) {
       renderChoice($questionTemplate, question.choices[i])
     }
     $('.container').html($questionTemplate)
   }
-  
+
   function renderChoice($questionTemplate, choice) {
     var $choiceTemplate = $($('#choice-template').html())
     var $choiceLink = $choiceTemplate.find('a')
-    $choiceLink.html(choice.choice).attr('href', '/questions/' + choice.question_id + '/answers.json').data('id', choice.choice_id).data('question_id', choice.question_id).data('quiz_id', choice.quiz_id)
+    $choiceLink.html(choice.choice).attr('href', '/questions/' + choice.question_id + '/answers.json').attr('id', choice.choice_id).attr('question_id', choice.question_id).attr('quiz_id', choice.quiz_id)
     $questionTemplate.append($choiceTemplate)
   }
 
+  function renderResults(data) {
+    var $resultsTemplate = $($('#results-template').html())
+    var $resultsField = ($($($resultsTemplate)[0]))
+    $($resultsField).html(data.status.correct + "<br>" + data.status.num_correct + "/" + data.status.num_incorrect )
+    $('.results').empty()
+    $('.results').append($resultsTemplate)
+
+  }
+
   return {
-    getNextQuestion: getNextQuestion
+    getFirstQuestion: getFirstQuestion,
+    displayResult: displayResult
   }
 }())
